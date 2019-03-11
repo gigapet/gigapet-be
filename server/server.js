f2e0f55da46fcb6ab5aee1cce8a189a15666ee37
Chiboba <briangfelix@gmail.com>
2019-03-11 10:13:27 -0700

    Adds proc

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+server.use(express.json())
+server.use(cors());
+
+server.get('/', (req,res) => {
+    console.log("HELLO");
+})
